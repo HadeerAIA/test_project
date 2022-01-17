46c5e66725506eef19067b25b0788f89f80621c5
esraa <esraa.mohamed.fcis@gmail.com>
2022-01-17 17:25:51 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/navbar/navbar';
+
+import React from 'react';
+function App() {
+
+  return (
+    <>
+   <h1>Hello</h1>
+   <Navbar test="mona"/>
+   </>
+  );
+}
+
+export default App;
